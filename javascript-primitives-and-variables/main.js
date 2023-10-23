a64c46a31bb1d0d2bfc85d5c386688d9c102b20c
Omid Asadi <omiddzedd@gmail.com>
2023-10-23 18:40:05 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Omid Asadi';
+let isCool = true;
+let totalPets = 0;
+
+console.log('fullName: ', fullName);
+console.log('isCool: ', isCool);
+console.log('totalPets: ', totalPets);
+
+console.log('fullName type: ', typeof fullName);
+console.log('isCool type: ', typeof isCool);
+console.log('totalPets type: ', typeof totalPets);
+
+let unknown = null;
+console.log('uknown:', unknown);
+console.log('unknown type: ', typeof unknown);
+
+let never;
+console.log('never: ', never);
+console.log('never type: ', typeof never);
