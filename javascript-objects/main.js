ff83dd8e6a2a6bfe2b61529a9c821537a99cca2b
Omid Asadi <omiddzedd@gmail.com>
2023-10-23 23:50:07 +0000

    Completed javascript-objects exercise.

---
+++ javascript-objects/main.js
@@ -0,0 +1,35 @@
+const student = {
+  firstName : 'Omid',
+  lastName : 'Asadi',
+  age : 36
+}
+
+const fullName = student.firstName + ' ' + student.lastName;
+console.log('Value of fullName: ', fullName);
+
+student.livesInIrvine = true;
+student.previousOccupation = 'amazon';
+
+console.log('value of student.livesInIrvine: ', student.livesInIrvine);
+console.log('value of student.previousOccupation: ', student.previousOccupation);
+
+const vehicle = {
+  make: 'Ford',
+  model: 'Boss 429',
+  year: 1969
+}
+
+vehicle.color = 'black';
+vehicle.isConvertible = false;
+console.log('Value of vehicle.color: ', vehicle.color);
+console.log('Value of vehicle.isConvertible: ', vehicle.isConvertible);
+console.log('Value of vehicle: ', vehicle);
+
+const pet = {
+  name: 'Rex',
+  type: 'Dog'
+}
+
+delete pet.name;
+delete pet.type;
+console.log('Value of pet: ', pet);
