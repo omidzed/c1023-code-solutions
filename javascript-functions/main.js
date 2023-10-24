2f4c2134d6286db85d0a379f63cb151640ef7c75
Omid Asadi <omiddzedd@gmail.com>
2023-10-24 23:22:03 +0000

    Corrections for javascript-functione exercise.

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -1,19 +1,26 @@
-function convertMinutesToSeconds(minutes){
+function convertMinutesToSeconds(minutes) {
   return minutes * 60;
 }
+console.log('Converted to seconds:', convertMinutesToSeconds(5));
 
 function greet(name) {
   return `Hey ${name}!`;
 }
+console.log('Greeting message:', greet('Bob'));
 
-function getArea(width, height){
+function getArea(width, height) {
   return width * height;
 }
+console.log('Area of rectangle:', getArea(2, 5));
 
-function getFirstName(person){
+function getFirstName(person) {
   return person.firstName;
 }
+const person = { firstName: 'Jack', lastName: 'Bauer' };
+console.log("Person's first name:", getFirstName(person));
 
-function getLastElement(array){
+function getLastElement(array) {
   return array[array.length - 1];
 }
+const array = [1, 2, 5, 500];
+console.log('Last element of this array:', getLastElement(array));
