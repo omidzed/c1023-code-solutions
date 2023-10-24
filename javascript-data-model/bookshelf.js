7021d9bb164769ed3adb71b5fb619e42bf247e62
Omid Asadi <omiddzedd@gmail.com>
2023-10-24 18:05:23 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,22 @@
+const bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett',
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson',
+  },
+  {
+    isbn: '978-0692232699',
+    title: "Build APIs You Won't Hate: Everyone and their dog wants an API, so you should probably learn how to build them",
+    author: 'Phil Sturgeon & Laura Bohill',
+  },
+];
+
+
+console.log('Author of 2nd book: ', bookshelf[1].author);
+console.log('ISBN of 1st book: ', bookshelf[0].isbn);
+console.log('Title of 3rd book: ', bookshelf[2].title);
