15d80ce8f13df0f0283985cdd4ec3910c36f1445
Omid Asadi <omiddzedd@gmail.com>
2023-10-30 16:13:20 +0000

    Iniital commit for object-manipulation-2.

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,16 @@
+/* exported pick */
+
+function pick(source, keys) {
+  const result = {};
+
+  let i = 0;
+  while (i < keys.length) {
+    const key = keys[i];
+    if (source[key] !== undefined) {
+      result[key] = source[key];
+      i++;
+console.log(result[key]);
+    } console.log(result);
+  }
+  return result;
+}
