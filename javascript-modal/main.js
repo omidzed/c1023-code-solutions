8eec2c5db80765c9bbeb17829807b064a73760a7
Omid Asadi <omiddzedd@gmail.com>
2023-11-08 13:43:13 +0000

    Completed javascript-modal challenge.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,12 +1,14 @@
 const $noButton = document.querySelector('.no');
+const $openModal = document.querySelector('.open-modal');
 const $container = document.querySelector('.container');
-
-
-$noButton.addEventListener('click', handleNoButton);
 
 function handleNoButton(event) {
   $container.classList.add('hidden');
-  console.log('no works');
 }
-console.log($noButton);
-console.log('running');
+
+function handleOpenModal(event) {
+  $container.classList.remove('hidden');
+}
+
+$openModal.addEventListener('click', handleOpenModal);
+$noButton.addEventListener('click', handleNoButton);
