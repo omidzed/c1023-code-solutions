0bc0b3f6f74c7edeabe0eb6359352a88531ffc5c
Omid Asadi <omiddzedd@gmail.com>
2023-11-21 16:22:26 +0000

    Completed javascript-carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,51 +1,102 @@
-const $views = document.querySelectorAll('.view-container');
+const $pikachuCircle = document.querySelector('#pikachu');
+const $charmanderCircle = document.querySelector('#charmander');
+const $squirtleCircle = document.querySelector('#squirtle');
+const $bulbasaurCircle = document.querySelector('#bulbasaur');
+const $jigglypuffCircle = document.querySelector('#jigglypuff');
+const $leftChevron = document.querySelector('.fa-chevron-left');
+const $rightChevron = document.querySelector('.fa-chevron-right');
+const $image = document.querySelector('#image');
 
-function viewSwap(targetView) {
-  for (let i = 0; i < $views.length; i++) {
-    if ($views[i].getAttribute('data-view') === targetView) {
-      $views[i].classList.remove('hidden');
-    } else {
-      $views[i].classList.add('hidden');
-    }
-  }
-  data.view = targetView;
-}
-
-const pokemon = [
-  {
-    number: '1',
-    name: 'pikachu',
-    imageUrl: 'images/pikachu.png',
-  },
-  {
-    number: '2',
-    name: 'bulbasaur',
-    imageUrl: 'images/bulbasaur.png',
-  },
-  {
-    number: '3',
-    name: 'charmander',
-    imageUrl: 'images/charmander.png',
-  },
-  {
-    number: '4',
-    name: 'squirtle',
-    imageUrl: 'images/squirtle.png',
-  },
-  {
-    number: '5',
-    name: 'jigglypuff',
-    imageUrl: 'images/jigglypuff.png',
-  },
+const images = [
+  './images/pikachu.png',
+  './images/bulbasaur.png',
+  './images/charmander.png',
+  './images/squirtle.png',
+  './images/jigglypuff.png',
 ];
 
-function renderPokemon(pokemon) {
-  const $columnChevron = document.createElement('div');
-  $columnChevron.setAttribute('class', 'column chevron');
+const circles = [
+  $pikachuCircle,
+  $charmanderCircle,
+  $squirtleCircle,
+  $bulbasaurCircle,
+  $jigglypuffCircle,
+];
 
-  const $chevronLeft = document.createElement('i');
-  $chevronLeft.setAttribute('class', 'fa-solid fa-chevron-left chevy');
+let currentIndex = 0;
 
-  const $chevronRight = document.createElement('i');
-  $chevronRight.setAttribute('class', 'fa-solid fa-chevron-right chevy');
+function changePokemon(direction = 1) {
+  currentIndex = (currentIndex + direction + images.length) % images.length;
+  $image.src = images[currentIndex];
+
+  for (let index = 0; index < circles.length; index++) {
+    const circle = circles[index];
+    if (index === currentIndex) {
+      circle.querySelector('i').className = 'fa-solid fa-circle';
+    } else {
+      circle.querySelector('i').className = 'fa-regular fa-circle';
+    }
+  }
+}
+
+$leftChevron.addEventListener('click', function (event) {
+  event.preventDefault();
+  changePokemon(-1);
+});
+
+$rightChevron.addEventListener('click', function (event) {
+  event.preventDefault();
+  changePokemon();
+});
+
+setInterval(changePokemon, 10000);
+
+circles[0].addEventListener('click', function (event) {
+  event.preventDefault();
+  $image.src = images[0];
+  circles[0].querySelector('i').className = 'fa-solid fa-circle';
+  changePokemon();
+  changeCircles();
+});
+
+circles[1].addEventListener('click', function (event) {
+  event.preventDefault();
+  $image.src = images[1];
+  circles[1].querySelector('i').className = 'fa-solid fa-circle';
+  changePokemon();
+  changeCircles();
+});
+
+circles[2].addEventListener('click', function (event) {
+  event.preventDefault();
+  $image.src = images[2];
+  circles[2].querySelector('i').className = 'fa-solid fa-circle';
+  changePokemon();
+  changeCircles();
+});
+
+circles[3].addEventListener('click', function (event) {
+  event.preventDefault();
+  $image.src = images[3];
+  circles[3].querySelector('i').className = 'fa-solid fa-circle';
+  changePokemon();
+  changeCircles();
+});
+
+circles[4].addEventListener('click', function (event) {
+  event.preventDefault();
+  $image.src = images[4];
+  circles[4].querySelector('i').className = 'fa-solid fa-circle';
+  changePokemon();
+  changeCircles();
+});
+
+function changeCircles() {
+  for (let i = 0; i < circles.length; i++) {
+    if ($image.src === images[i]) {
+      circles[i].querySelector('i').className = 'fa-solid fa-circle';
+    } else {
+      circles[i].querySelector('i').className = 'fa-regular fa-circle';
+    }
+  }
 }
