0e54b6d27bb87746f58e815812d374e9083f9111
Omid Asadi <omiddzedd@gmail.com>
2023-11-21 21:42:37 +0000

    initial commit.

---
+++ javascript-new/new-objects.js
@@ -0,0 +1,32 @@
+// In the provided new- objects.js, complete the following tasks for each of these built -in JavaScript constructors:
+// Object, Array, Function, String, Number, and Boolean:
+
+// Call the constructor with new and assign the return value to a variable.
+
+// e.g. const anObject = new Object();
+
+// Log the value of the instance to the console.
+
+// e.g. console.log('value of anObject:', anObject);
+
+// Log the typeof the instance to the console.
+
+// e.g. console.log('typeof anObject:', typeof anObject);
+
+// repeat for Array, Function, String, Number, and Boolean.
+
+const newObject = new Object();
+console.log('Value of newObject:', newObject);
+console.log('typeof newObject:', typeof newObject);
+
+const anArray = new Array();
+console.log('Value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+const todaysFunction = new Function();
+console.log('Value of todaysFunction:', todaysFunction);
+console.log('typeof todaysFunction :', typeof todaysFunction);
+
+const anArray = new Array();
+console.log('Value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
