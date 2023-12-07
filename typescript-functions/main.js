98624a44d4b928f8bbbdce97b809014b0ad8474a
Omid Asadi <omiddzedd@gmail.com>
2023-12-07 22:25:26 +0000

    saving works, will return to complete after unit tests exercise

---
+++ typescript-functions/main.js
@@ -0,0 +1,39 @@
+// function convertMinutesToSeconds(minutes: number) {
+//   const seconds: number = minutes * 60;
+//   return seconds;
+// }
+// const convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+// console.log('convertMinutesToSecondsResult',convertMinutesToSecondsResult );
+// function greet(name: string) {
+//   const greeting: string = 'Hi ' + name + '!';
+//   return greeting;
+// }
+// const greetResult = greet('Bob');
+// console.log('Greeting message:', greetResult);
+// function getArea(width: number, height: number) {
+//   const area: number = width * height;
+//   return area;
+// }
+// console.log('Area of rectangle:', getArea(2, 5));
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+// function getFirstName(person: Person) {
+//   const firstName: string;
+//   return firstName;
+// }
+// const person = { firstName: 'Jack', lastName: 'Bauer' };
+// console.log("Person's first name:", getFirstName(person));
+// function getLastElement(array: string[]): string {
+//   const lastIndex: number = array.length - 1;
+//   const lastElement: string = array[lastIndex];
+//   return lastElement;
+// }
+// const getLastElementResult: string = getLastElement([
+//   'propane',
+//   'and',
+//   'propane',
+//   'accessories',
+// ]);
+// console.log('getLastElementResult:', getLastElement)
