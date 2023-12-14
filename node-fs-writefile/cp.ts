da956ffe8dacc76766a0bd12935abbcb80c58cf3
Omid Asadi <omiddzedd@gmail.com>
2023-12-14 06:37:19 +0000

    Will finish exercise shortly.

---
+++ node-fs-writefile/cp.ts
@@ -0,0 +1,16 @@
+// import { readFile, writeFile } from 'node:fs/promises';
+
+// try {
+//   await readFile('node-fs-writefile/note.txt);
+//   console.log('source.txt was copied to destination.txt');
+// } catch {
+//   console.error('The file could not be copied');
+// }
+
+// // By using COPYFILE_EXCL, the operation will fail if destination.txt exists.
+// try {
+//   await copyFile('source.txt', 'destination.txt', constants.COPYFILE_EXCL);
+//   console.log('source.txt was copied to destination.txt');
+// } catch {
+//   console.error('The file could not be copied');
+// }
---
+++ node-fs-writefile/note-to-self.ts
@@ -0,0 +1,10 @@
+import { writeFile } from 'node:fs/promises';
+
+// const note = process.argv[2];
+const [, , note] = process.argv;
+
+try {
+  await writeFile('note.txt', note + '\n', 'utf8');
+} catch (err) {
+  console.error('error message', err);
+}
