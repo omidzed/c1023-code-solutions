2f74c353de59fdc711cb85385ffe6c44c1d85c4e
Omid Asadi <omiddzedd@gmail.com>
2023-12-18 21:52:06 +0000

    Completed express-error-handling exercise.

---
+++ express-error-handling/error-middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import { ClientError } from './client-error.js';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function errorMiddleware(
+  err: unknown,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- next must be declared for error middleware
+  next: NextFunction
+) {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    console.error(err);
+    res.status(500).json({ error: 'an unexpected error occurred' });
+  }
+}
