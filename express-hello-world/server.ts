8473e0c837d6904c16f6bf4bac042de5922096e4
Omid Asadi <omiddzedd@gmail.com>
2023-12-18 01:55:05 +0000

    Fixed Package.json and the app.use()

--- express-hello-world/server.ts
+++ express-hello-world/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
-app.listen(8080, () => {
-  console.log('Express server listening on port 8080');
+
+app.use((req, res) => {
+  console.log(req.method);
+  res.send('Hello World');
 });
 
-app.use((req, res, next) => {
-  console.log('Time:', Date.now());
-  next();
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
 });
