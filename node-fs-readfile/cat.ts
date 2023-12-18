9ef40e4225cf4a40b62d3f4040dbd0157a7fdeb6
Omid Asadi <omiddzedd@gmail.com>
2023-12-18 05:26:12 +0000

    Completed node-fs-readfile exercise.

--- node-fs-readfile/cat.ts
+++ node-fs-readfile/cat.ts
@@ -5,7 +5,7 @@ const filenames = process.argv.slice(2);
 try {
   const promises = filenames.map((path) => readFile(path, 'utf8'));
   const contents = await Promise.all(promises);
-  console.log(contents.join('\nNext File:\n'));
+  console.log(contents.join('\n'));
 } catch (err) {
   console.log(err);
   process.exit(1);
