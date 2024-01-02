5fd619ca42266f4441c40a130b803e295259f9e7
Omid Asadi <omiddzedd@gmail.com>
2024-01-02 13:11:47 +0000

    Saving progress

--- react-search-component/my-app/src/SearchBox.tsx
+++ react-search-component/my-app/src/SearchBox.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 export function SeachBox() {
+  const [placeholder, setPlaceholder] = useState('Search...');
+
+  function handleClick() {
+    setPlaceholder('');
+  }
+
+  function handleBlur() {
+    setPlaceholder('Search...');
+  }
+
   return (
     <div className="search-box">
-      <input id="myTextarea" placeholder="Search... "></input>
+      <input
+        id="myTextarea"
+        placeholder={placeholder}
+        onClick={handleClick}
+        onBlur={handleBlur}></input>
     </div>
   );
 }
