3e5eef6c09d2ccae463268da750d8d725033c603
Omid Asadi <omiddzedd@gmail.com>
2024-01-05 19:41:48 +0000

    saving progress, need to fix overlay and heading

--- react-app-drawer-component/my-app/src/AppDrawer.tsx
+++ react-app-drawer-component/my-app/src/AppDrawer.tsx
@@ -1,6 +1,6 @@
 import { Menu } from './Menu';
 import { useState } from 'react';
-// import { OverlayShade } from './OverlayShade';
+import { OverlayShade } from './OverlayShade';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 type AppDrawerProps = {
@@ -10,26 +10,22 @@ type AppDrawerProps = {
 export function AppDrawer({ menuItems }: AppDrawerProps) {
   const [isOpen, setIsOpen] = useState(false);
 
-  function handleClick() {
+  function toggleDrawer() {
     setIsOpen(!isOpen);
   }
 
-  // function handleOverlayClick() {
-  //   setIsOpen(!isOpen);
-  // }
-
   return (
-    <div className={'drawer-container'}>
-      <div className={isOpen ? 'hidden' : 'hamburger-container'}>
-        <GiHamburgerMenu size={35} onClick={handleClick} />
-      </div>
-      <div className={isOpen ? 'menu-drawer' : 'menu-drawer closed'}>
-        <Menu menuItems={menuItems} />
-        {/* <OverlayShade
-          onOverlayClick={handleOverlayClick}
-          isOpen={isOpen ? 'overlay-shade' : 'hidden'}
-        /> */}
+    <div className="wrapper">
+      <div className={'drawer-container'}>
+        <div className={isOpen ? 'hidden' : 'hamburger-container'}>
+          <GiHamburgerMenu size={35} onClick={toggleDrawer} />
+        </div>
+        <div className={isOpen ? 'menu-drawer open' : 'menu-drawer closed'}>
+          <Menu menuItems={menuItems} />
+        </div>
+        <OverlayShade toggle={toggleDrawer} isOpen={isOpen} />
       </div>
     </div>
   );
 }
+//  <h1 className="heading">heading</h1>
--- react-app-drawer-component/my-app/src/OverlayShade.tsx
+++ react-app-drawer-component/my-app/src/OverlayShade.tsx
@@ -1,7 +1,12 @@
-// type OverlayShadeProps = {
-//   isOpen: string;
-//   handleOverlayClick: () => void;
-// };
-// export function OverlayShade({ isOpen, handleOverlayClick }: OverlayShadeProps) {
-//   return <div className={isOpen} onOverlayClick={handleOverlayClick}></div>;
-// }
+type OverlayShadeProps = {
+  isOpen: boolean;
+  toggle: (event: React.MouseEvent<HTMLDivElement>) => void;
+};
+
+export function OverlayShade({ isOpen, toggle }: OverlayShadeProps) {
+  return (
+    <div
+      className={isOpen ? 'overlay-shade' : 'overlay-shade hidden'}
+      onClick={toggle}></div>
+  );
+}
